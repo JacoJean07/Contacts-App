b82bbe9f7211c88a05bd37005cbcce24f2f80bcc
Jaco Jeanuwu <you@example.com>
2023-12-19 20:30:54 -0500

    creando el setup.sql para test de base de datos

---
+++ sql/setup.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS contacts_app;
+
+CREATE DATABASE contacts_app;
+
+USE contacts_app;
+
+CREATE TABLE contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    phone_number VARCHAR(255)
+)
+
