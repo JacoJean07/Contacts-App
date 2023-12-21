5c89b83020b03f23801e8480d407b850805069a1
Jaco Jeanuwu <you@example.com>
2023-12-20 19:18:56 -0500

    creando el delete contact + cambio en index

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+
+require "database.php";
+// USAREMOS EL METODO GET PARA BUSCAR EL ROW QUE VAMOS A ELIMINAR
+$id = $_GET["id"];
+//PRIMERO LO SOLICITAMOS A LA BASE DE DATOS
+$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+$statement->execute([":id" => $id]);
+//COMPROBAMOS QUE EL ID EXISTA, EN CASO DE QUE EL USUARIO NO SEA UN NAVEGADOR, Y SI NO EXISTE EL ID MANDAMOS UN ERROR
+if ($statement->rowCount() == 0) {
+  http_response_code(404);
+  echo("HTTP 404 NOT FOUND (ID NO ENCONTRADO)");
+}
+
+//ELIMINAMOS EL ROW CON EL ID DE LA TARGETA SELECCIONADA
+$conn->prepare("DELETE FROM contacts WHERE id = :id")->execute([":id" => $id]);
+//REDIRIGIMOS AL INDEX.PHP
+header("Location: index.php");
+
+?>
--- index.php
+++ index.php
@@ -81,7 +81,8 @@ $contacts = $conn->query("SELECT * FROM contacts");
                 <h3 class="card-title text-capitalize"> <?= $contact["name"]?> </h3>
                 <p class="m-2"><?= $contact["phone_number"]?></p>
                 <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
-                <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
+                <!-- LLAMAMOS AL DELETE.PHP Y LE ASIGNAMOS EL ID CON UN ARRAY ASOCIATIVO AL ID QUE PERTENEZCA EL CONTACTO EN EL BUCLE FOREACH -->
+                <a href="delete.php?id=<? $contact["id"] ?>" class="btn btn-danger mb-2">Delete Contact</a>
               </div>
             </div>
           </div>
